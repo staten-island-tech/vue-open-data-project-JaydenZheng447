22b9a0adecdc57c55755e2e02ebb3366a46ef9d2
JaydenZheng447 <jaydenzheng447@gmail.com>
2026-04-12 21:01:38 -0400

    fixed bar charts for pokemoncard and pokemondata

--- app/src/views/PokemonData.vue
+++ app/src/views/PokemonData.vue
@@ -3,12 +3,15 @@
     <h2>Bin: {{ pokemon?.bin }}</h2>
     <p>Organization: {{ pokemon?.organization_name }}</p>
     <p>Address: {{ pokemon?.organization_address }}</p>
+    <BarChart :data="chartData3" />
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
+
+import { computed } from 'vue'
 const route = useRoute()
 const pokemon = ref(null)
 async function getPokemon(bin) {
@@ -30,6 +33,32 @@ watch(
 onMounted(function () {
   getPokemon(route.params.bin)
 })
+
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+
+import BarChart from '../views/BarChart.vue'
+
+const chartData3 = computed(() => ({
+  labels: ['amount_requested', 'bin', 'census_tract'],
+  datasets: [
+    {
+      data: [
+        Number(pokemon?.amount_requested) || 0,
+        Number(pokemon?.zip) || 0,
+        Number(pokemon?.census_tract) || 0,
+      ],
+    },
+  ],
+}))
 </script>
 
 <style scoped></style>
